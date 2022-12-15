10f6fe0b8e99303d6ead341a7c4bf939ae93b83f
glottis <skin_tiicket@hotmail.com>
2022-12-15 22:44:16 +0100

    use fetch instead of get

--- config/config.exs
+++ config/config.exs
@@ -3,14 +3,14 @@ config :tz, reject_time_zone_periods_before_year: 2022
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
 config :weather_elixir, :emqtt,
-  host: System.get_env("MQTT_HOST") |> String.to_charlist(),
-  port: System.get_env("MQTT_PORT") |> String.to_integer(),
-  clientid: System.get_env("MQTT_USER"),
-  username: System.get_env("MQTT_USER"),
-  password: System.get_env("MQTT_USER_PW"),
+  host: System.fetch_env!("MQTT_HOST") |> String.to_charlist(),
+  port: System.fetch_env!("MQTT_PORT") |> String.to_integer(),
+  clientid: System.fetch_env!("MQTT_USER"),
+  username: System.fetch_env!("MQTT_USER"),
+  password: System.fetch_env!("MQTT_USER_PW"),
   ssl: true,
   ssl_opts: [
-    server_name_indication: System.get_env("MQTT_SNI") |> String.to_charlist(),
-    cacertfile: System.get_env("CACERT")
+    server_name_indication: System.fetch_env!("MQTT_SNI") |> String.to_charlist(),
+    cacertfile: System.fetch_env!("CACERT")
   ],
   name: :emqtt
