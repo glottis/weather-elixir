dbe1e4aae0faf849346a4e1c7beb6c3f6c8d4891
glottis <skin_tiicket@hotmail.com>
2023-01-06 01:39:47 +0100

    zzz typo

--- config/config.exs
+++ config/config.exs
@@ -3,10 +3,10 @@ config :tz, reject_time_zone_periods_before_year: 2023
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 
 config :weather_elixir,
-    host: System.fetch_env!("MQTT_HOST") |> String.to_charlist(),
-    port: System.fetch_env!("MQTT_PORT") |> String.to_integer(),
-    sni: System.fetch_env!("MQTT_SNI") |> String.to_charlist(),
-    ca: System.fetch_env!("CACERT")
-    clientid: System.fetch_env!("MQTT_USER"),
-    username: System.fetch_env!("MQTT_USER"),
-    password: System.fetch_env!("MQTT_USER_PW")
+  host: System.fetch_env!("MQTT_HOST") |> String.to_charlist(),
+  port: System.fetch_env!("MQTT_PORT") |> String.to_integer(),
+  sni: System.fetch_env!("MQTT_SNI") |> String.to_charlist(),
+  ca: System.fetch_env!("CACERT"),
+  clientid: System.fetch_env!("MQTT_USER"),
+  username: System.fetch_env!("MQTT_USER"),
+  password: System.fetch_env!("MQTT_USER_PW")
